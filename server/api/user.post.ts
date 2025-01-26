ef41b36833e37cfee17e36465b2f0d499ef21af2
amer-wild11 <amer33wild@gmail.com>
2025-01-26 03:51:09 +0300

    Test version

--- server/api/user.post.ts
+++ server/api/user.post.ts
@@ -1,4 +1,6 @@
-import prisma from "~/lib/prisma.server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
